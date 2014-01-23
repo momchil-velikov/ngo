581d44e0a4653090f95f6fcd2c83e0d06b6e14fa
Momchil Velikov <momchil.velikov@gmail.com>
2014-01-24 00:29:51 +0200

    Add scanfilt - a tool to aid scanner testing

---
+++ src/scanfilt/dfilt.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+dir=$1
+
+for f in $(find ${dir} -name '*.go'); do
+    echo "$f"
+    ./bin/scanfilt < $f > ${f}.scanfilt.1
+done
+
+for f in $(find ${dir} -name '*.go.scanfilt.1'); do
+    dst=$(dirname $f)/$(basename -s .scanfilt.1 ${f}).scanfilt.2
+    echo "$f"
+    ./bin/scanfilt < $f > ${dst}
+done
+
+for f1 in $(find ${dir} -name '*.go.scanfilt.1'); do
+    f2=$(dirname ${f1})/$(basename -s .scanfilt.1 ${f1}).scanfilt.2
+    diff -ud ${f1} ${f2}
+done
---
+++ src/scanfilt/scanfilt.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"golang/scanner"
+	"io"
+	"io/ioutil"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func escape_rune(in rune) string {
+	return fmt.Sprintf("\\U%08x", in)
+}
+
+func escape_byte(in byte) string {
+	return fmt.Sprintf("\\x%02x", in)
+}
+
+func escape(in string, str bool) string {
+	var out []byte
+	for off, r := range in {
+		if r == '\\' {
+			out = append(out, "\\\\"...)
+		} else if r == '"' && str {
+			out = append(out, "\\\""...)
+		} else if r == '\'' && !str {
+			out = append(out, "\\'"...)
+		} else if r == utf8.RuneError {
+			_, sz := utf8.DecodeRuneInString(in[off:])
+			if sz == 1 {
+				out = append(out, escape_byte(in[off])...)
+			} else {
+				out = append(out, escape_rune(r)...)
+			}
+		} else if unicode.IsPrint(r) {
+			out = append(out, in[off:off+utf8.RuneLen(r)]...)
+		} else {
+			out = append(out, escape_rune(r)...)
+		}
+	}
+
+	return string(out)
+}
+
+func transcan(in io.Reader, out io.Writer) {
+	src, err := ioutil.ReadAll(in)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "scanfilt: ", err)
+	}
+
+	var tok uint
+	s := scanner.New("stdin", string(src))
+	for tok = s.Get(); tok != scanner.EOF && tok != scanner.ERROR; tok = s.Get() {
+		switch tok {
+		case scanner.ID, scanner.INTEGER, scanner.FLOAT:
+			fmt.Fprint(out, s.Value, " ")
+		case scanner.IMAGINARY:
+			fmt.Fprint(out, s.Value, "i ")
+		case scanner.STRING:
+			fmt.Fprintf(out, "\"%s\" ", escape(s.Value, true))
+		case scanner.RUNE:
+			fmt.Fprintf(out, "'%s' ", escape(s.Value, false))
+		// case ';':
+		// 	fmt.Fprint(out, "\n")
+		default:
+			fmt.Fprint(out, scanner.TokenNames[tok], " ")
+		}
+	}
+
+	if tok == scanner.ERROR {
+		fmt.Fprint(os.Stderr, "\nscanfilt: ", s.Err, "\n")
+	}
+}
+
+func main() {
+	transcan(os.Stdin, os.Stdout)
+}
