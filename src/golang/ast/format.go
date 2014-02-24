54f53b802d88c526270de5459a3cfc568fe3a5c9
Momchil Velikov <momchil.velikov@gmail.com>
2014-02-24 23:30:23 +0200

    Export Format methods from the ast package

--- src/golang/ast/format.go
+++ src/golang/ast/format.go
@@ -3,7 +3,7 @@ package ast
 // import "fmt"
 
 type Formatter interface {
-    format(uint) string
+    Format(uint) string
 }
 
 // Return a string, consisting of N spaces.
@@ -20,25 +20,25 @@ func (f *File) Format() (s string) {
 
     if len(f.Imports) > 0 {
         if len(f.Imports) == 1 {
-            s += "import " + f.Imports[0].format(0) + "\n"
+            s += "import " + f.Imports[0].Format(0) + "\n"
         } else {
             s += "import (\n"
             for _, i := range f.Imports {
-                s += i.format(1) + "\n"
+                s += i.Format(1) + "\n"
             }
             s += ")\n\n"
         }
     }
 
     for _, d := range f.Decls {
-        s += d.format(0)
+        s += d.Format(0)
     }
 
     return
 }
 
 // Output formatted import clause with N levels of indentation.
-func (i *Import) format(n uint) (s string) {
+func (i *Import) Format(n uint) (s string) {
     s = nspaces(4 * n)
     if len(i.Name) > 0 {
         s += i.Name + " "
@@ -48,14 +48,14 @@ func (i *Import) format(n uint) (s string) {
 }
 
 // Output a formatted type declaration.
-func (t *TypeDecl) format(n uint) (s string) {
+func (t *TypeDecl) Format(n uint) (s string) {
     s = "type " + t.Name + " "
-    s += t.Type.format(n) + "\n\n"
+    s += t.Type.Format(n) + "\n\n"
     return
 }
 
 // Output a formatter constant group declaration
-func (c *ConstGroup) format(n uint) string {
+func (c *ConstGroup) Format(n uint) string {
     indent := nspaces(4 * n)
     s := indent + "const (\n"
     for _, d := range c.Decls {
@@ -66,7 +66,7 @@ func (c *ConstGroup) format(n uint) string {
 }
 
 // Output a formatted constant declaration.
-func (c *ConstDecl) format(n uint) string {
+func (c *ConstDecl) Format(n uint) string {
     return c.format_internal(n, true) + "\n"
 }
 
@@ -81,19 +81,19 @@ func (c *ConstDecl) format_internal(n uint, top bool) (s string) {
         s += ", " + c.Names[i]
     }
     if c.Type != nil {
-        s += " " + c.Type.format(n+1)
+        s += " " + c.Type.Format(n+1)
     }
     if k := len(c.Values); k > 0 {
-        s += " = " + c.Values[0].format(n+1)
+        s += " = " + c.Values[0].Format(n+1)
         for i := 1; i < k; i++ {
-            s += ", " + c.Values[i].format(n+1)
+            s += ", " + c.Values[i].Format(n+1)
         }
     }
     return s
 }
 
 // Output a formatted variable group declaration
-func (c *VarGroup) format(n uint) string {
+func (c *VarGroup) Format(n uint) string {
     indent := nspaces(4 * n)
     s := indent + "var (\n"
     for _, d := range c.Decls {
@@ -104,7 +104,7 @@ func (c *VarGroup) format(n uint) string {
 }
 
 // Output a formatted variable declaration.
-func (c *VarDecl) format(n uint) string {
+func (c *VarDecl) Format(n uint) string {
     return c.format_internal(n, true) + "\n"
 }
 
@@ -119,46 +119,46 @@ func (c *VarDecl) format_internal(n uint, top bool) (s string) {
         s += ", " + c.Names[i]
     }
     if c.Type != nil {
-        s += " " + c.Type.format(n+1)
+        s += " " + c.Type.Format(n+1)
     }
     if k := len(c.Init); k > 0 {
-        s += " = " + c.Init[0].format(n+1)
+        s += " = " + c.Init[0].Format(n+1)
         for i := 1; i < k; i++ {
-            s += ", " + c.Init[i].format(n+1)
+            s += ", " + c.Init[i].Format(n+1)
         }
     }
     return s
 }
 
 // Output a formatted function declaration.
-func (f *FuncDecl) format(n uint) string {
+func (f *FuncDecl) Format(n uint) string {
     s := "func"
     if f.Recv != nil {
-        s += " " + f.Recv.format(n+1)
+        s += " " + f.Recv.Format(n+1)
     }
     s += " " + f.Name + format_signature(f.Sig, n+1)
     if f.Body != nil {
-        s += f.Body.format(n + 1)
+        s += f.Body.Format(n + 1)
     }
     return s + "\n"
 }
 
 // Output a formatted block
-func (b *Block) format(n uint) string {
+func (b *Block) Format(n uint) string {
     return " {\n}"
 }
 
 // Output a formatter method receiver.
-func (r *Receiver) format(n uint) string {
+func (r *Receiver) Format(n uint) string {
     s := "("
     if len(r.Name) > 0 {
         s += r.Name + " "
     }
-    return s + r.Type.format(n+1) + ")"
+    return s + r.Type.Format(n+1) + ")"
 }
 
 // Output a formatted type.
-func (t *QualId) format(n uint) (s string) {
+func (t *QualId) Format(n uint) (s string) {
     if len(t.Pkg) > 0 {
         s += t.Pkg + "."
     }
@@ -166,38 +166,38 @@ func (t *QualId) format(n uint) (s string) {
     return
 }
 
-func (t *ArrayType) format(n uint) (s string) {
-    return "[" + t.Dim.format(n+1) + "]" + t.EltType.format(n)
+func (t *ArrayType) Format(n uint) (s string) {
+    return "[" + t.Dim.Format(n+1) + "]" + t.EltType.Format(n)
 }
 
-func (t *SliceType) format(n uint) (s string) {
-    return "[]" + t.EltType.format(n)
+func (t *SliceType) Format(n uint) (s string) {
+    return "[]" + t.EltType.Format(n)
 }
 
-func (t *PtrType) format(n uint) string {
-    return "*" + t.Base.format(n)
+func (t *PtrType) Format(n uint) string {
+    return "*" + t.Base.Format(n)
 }
 
-func (t *MapType) format(n uint) string {
-    return "map[" + t.KeyType.format(0) + "]" + t.EltType.format(n)
+func (t *MapType) Format(n uint) string {
+    return "map[" + t.KeyType.Format(0) + "]" + t.EltType.Format(n)
 }
 
-func (t *ChanType) format(n uint) (s string) {
+func (t *ChanType) Format(n uint) (s string) {
     if !t.Send {
         s += "<-"
     }
     s += "chan"
     if !t.Recv {
-        s += "<- " + t.EltType.format(n)
+        s += "<- " + t.EltType.Format(n)
     } else if ch, ok := t.EltType.(*ChanType); ok && !ch.Send {
-        s += " (" + ch.format(n) + ")"
+        s += " (" + ch.Format(n) + ")"
     } else {
-        s += " " + t.EltType.format(n)
+        s += " " + t.EltType.Format(n)
     }
     return
 }
 
-func (t *StructType) format(n uint) string {
+func (t *StructType) Format(n uint) string {
     if len(t.Fields) == 0 {
         return "struct{}"
     }
@@ -209,7 +209,7 @@ func (t *StructType) format(n uint) string {
         if len(f.Name) > 0 {
             s += f.Name + " "
         }
-        s += f.Type.format(n + 1)
+        s += f.Type.Format(n + 1)
         if len(f.Tag) > 0 {
             s += " \"" + f.Tag +
                 "\""
@@ -221,7 +221,7 @@ func (t *StructType) format(n uint) string {
     return s
 }
 
-func (t *FuncType) format(n uint) string {
+func (t *FuncType) Format(n uint) string {
     return "func" + format_signature(t, n)
 }
 
@@ -235,7 +235,7 @@ func format_signature(t *FuncType, n uint) (s string) {
 
     k = len(t.Returns)
     if k == 1 && len(t.Returns[0].Name) == 0 {
-        s += " " + t.Returns[0].Type.format(n+1)
+        s += " " + t.Returns[0].Type.Format(n+1)
     } else if k > 0 {
         s += " (" + format_params(t.Returns, n) + ")"
     }
@@ -249,7 +249,7 @@ func format_params(p []*ParamDecl, n uint) (s string) {
     if p[0].Variadic {
         s += "..."
     }
-    s += p[0].Type.format(n + 1)
+    s += p[0].Type.Format(n + 1)
     for i, k := 1, len(p); i < k; i++ {
         s += ", "
         if len(p[i].Name) > 0 {
@@ -258,19 +258,19 @@ func format_params(p []*ParamDecl, n uint) (s string) {
         if p[i].Variadic {
             s += "..."
         }
-        s += p[i].Type.format(n + 1)
+        s += p[i].Type.Format(n + 1)
     }
     return
 }
 
-func (t *InterfaceType) format(n uint) string {
+func (t *InterfaceType) Format(n uint) string {
     if len(t.Embed) == 0 && len(t.Methods) == 0 {
         return "interface{}"
     }
     s := "interface {\n"
     indent := nspaces(4 * (n + 1))
     for _, e := range t.Embed {
-        s += indent + e.format(n+1) + "\n"
+        s += indent + e.Format(n+1) + "\n"
     }
     for _, m := range t.Methods {
         s += indent + m.Name + format_signature(m.Sig, n+1) + "\n"
@@ -280,6 +280,6 @@ func (t *InterfaceType) format(n uint) string {
 }
 
 // Output a formatted expression
-func (e *Operand) format(n uint) string {
+func (e *Operand) Format(n uint) string {
     return e.Const
 }
