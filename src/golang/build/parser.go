218893dd4a1a8010a6b2e2fd7b3287da3b80d8d0
Momchil Velikov <momchil.velikov@gmail.com>
2015-11-17 12:17:10 +0200

    golang/build: Declare Package and File types in a separate source file

--- src/golang/build/parser.go
+++ src/golang/build/parser.go
@@ -19,23 +19,6 @@ type parser struct {
 	skipc bool
 }
 
-// Package
-type Package struct {
-	Dir     string // Full pathname of the package directory
-	Path    string // Import path of the package
-	Name    string // Package name
-	Files   []*File
-	Imports map[string]*Package
-	Mark    int
-}
-
-// Source file
-type File struct {
-	Path, Package string
-	Imports       []string
-	Comments      []string
-}
-
 func (p *parser) init(name string, rd io.RuneReader) {
 	p.scan.init(name, rd)
 }
